7e35aad78329152aa3d921345d19533e8b6cdd37
Warrieuse <61115952+Warrieuse@users.noreply.github.com>
2020-09-11 16:10:04 +0200

    OK

---
+++ Exercice 10 - addEventListener/main.js
@@ -0,0 +1,6 @@
+
+document.getElementById('bouton1').addEventListener('click', function () {
+    document.getElementById('bouton1').innerHTML = "Vous m'avez cliqué";
+    document.getElementById('bouton1').style.color="red";
+})
+
