763b1e582548e2c5cf366475b87e20455f4c8a2b
Warrieuse <61115952+Warrieuse@users.noreply.github.com>
2020-09-11 14:39:52 +0200

    OK

---
+++ Exercice 9 - Read more/main.js
@@ -0,0 +1,15 @@
+let vDivShort = document.getElementById("div_short");
+let vDivMore = document.getElementById("div_More");
+
+
+function fReadMore() {
+    if (vDivShort.display="block"){  
+            vDivShort.style.display = "none";
+            vDivMore.style.display = "block";
+     } else {
+             vDivShort.style.display = "block";
+            vDivMore.style.display = "none";
+     }}; 
+
+// no commentaire très rare de ma part mais la franchement 
+// on peut dire que le code se suffit à lui même ...
